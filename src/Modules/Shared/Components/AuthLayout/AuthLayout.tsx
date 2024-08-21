480dc7ca4cfc3e089bfbb04a0130b250054ebbe6
amalshaban <amalshaban0904@gmail.com>
2024-08-21 11:29:09 +0300

    first commit

--- src/Modules/Shared/Components/AuthLayout/AuthLayout.tsx
+++ src/Modules/Shared/Components/AuthLayout/AuthLayout.tsx
@@ -1,12 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 import logo from '../../../../assets/images/PMS3.png'
-export default function AuthLayout() {
+
+
+
+
+export default function AuthLayout({title, subTitle}) {
+
   return (
-    <div className="auth-container d-flex justify-content-center align-items-center">
-      <div className=" ">
-        <img className="" src={logo}></img>
-        <Outlet />
+    <>
+    <div className="auth-container d-flex justify-content-center  align-items-center">
+      <div className="w-50 text-center">
+        <img className='img-fluid' src={logo}></img>
+        
+        <Outlet/>
       </div>
-    </div>
+       
+      </div>
+    
+    </>
+    
   )
 }
