49f45845409b5716568122313e5286e85d796db1
amalshaban <amalshaban0904@gmail.com>
2024-09-05 12:16:39 +0300

    users

--- src/Modules/Users/Components/UsersList/UsersList.tsx
+++ src/Modules/Users/Components/UsersList/UsersList.tsx
@@ -5,20 +5,20 @@ import NoData from "../../../Shared/Components/NoData/NoData";
 
 export default function UsersList() {
   const [userlist,Setuserlist]=useState([])
-  const userslist = async(pageSize: number,pageNumber: number)=>{
+  const getuserslist = async(pageSize: number,pageNumber: number)=>{
     try {
-      let response = await axios.get(USERS_URLs.TotalManager,AuthorizedTokenWithParam("",pageSize,pageNumber));
-    
+      const response = await axios.get("https://upskilling-egypt.com:3003/api/v1/Users/Manager",
+        AuthorizedTokenWithParam("",pageSize,pageNumber));
+    console.log(response);
       Setuserlist(response.data.data);
-      console.log(userlist)
-      console.log(response)
+      console.log(userlist);
     } 
     catch(error) {
       console.log(error);
     }
   }
 useEffect(() => {
-  userslist(1,1)
+  getuserslist(10,1);
 }, [])
   return (
     <>
@@ -31,20 +31,24 @@ useEffect(() => {
   <table className="table table-striped">
   <thead>
     <tr>
-      <th scope="col">Title</th>
-      <th scope="col">Discription</th>
-      <th scope="col">Creation Date</th>
-      <th scope="col">Modification Date</th>
+      <th scope="col">User</th>
+      <th scope="col">Email</th>
+      <th scope="col">Country</th>
+    
       <th scope="col"></th>
     </tr>
   </thead>
   <tbody>
-{/* {userlist.map((user:any)=>(
+{userlist.map((user:any)=>(
   <tr key={user.id}>
     
-    <td>{user.title}</td>
-    <td>{user.description}</td>
-    <td>
+    <td>{user.userName}</td>
+    <td>{user.email}</td>
+    <td>{user.country}</td>
+  
+    
+    
+    {/* <td>
     
     <i className="fa-solid fa-ellipsis-vertical menu" onClick={()=>handelmenuetoggle(user.id)}></i>
     {itemselectid === user.id && (
@@ -52,13 +56,13 @@ useEffect(() => {
                         <ul className="list-unstyled m-0">
                           <li className="dropdown-item"><i className="fa fa-eye"></i> Show</li>
                           <li className="dropdown-item"><i className="fa fa-edit"></i> Edit</li>
-                          <li className="dropdown-item" onClick={deleteitem}><i className="fa fa-trash"></i> Delete</li>
+                          <li className="dropdown-item" ><i className="fa fa-trash"></i> Delete</li>
                         </ul>
                       </div>
                     )}
-   </td>
+   </td> */}
   </tr>
-))} */}
+))}
   
   </tbody>
 </table>:<NoData/>} 
